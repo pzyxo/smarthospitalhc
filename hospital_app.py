33066f0e1a4f66f77e6eded6533b741694bb2f5b
pzyxo <63105421+pzyxo@users.noreply.github.com>
2026-06-25 15:53:03 +0700

    Change text color from blue to white in markdown

--- hospital_app.py
+++ hospital_app.py
@@ -68,7 +68,7 @@ DEPT_INFO = {
 
 st.markdown("""
 <div style="padding: 3rem 2rem 2.5rem;margin: -1rem 2rem; text-align:center;">
-  <div style="font-size:14px; color:blue;text-transform:uppercase;margin-bottom:12px;">
+  <div style="font-size:14px; color:white;text-transform:uppercase;margin-bottom:12px;">
   Future Classroom : Machine Learning
   <br>
   Smart Hospital by Roy
